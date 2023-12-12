6ada70c6a8e73de405d6271dc62573e81f8d82f7
nicknamed <nicknamed19@gmail.com>
2023-12-12 16:44:27 -0500

    Se trae del contexto las variables a utilizar para renderizar las paginas que correspondan

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,23 +5,43 @@ import { Card } from "../../Components/Card";
 import { SearchProducts } from "../../Components/SearchProducts";
 
 function Home() {
-    const { searcheadItems } = useContext(ShoppingCartContext)
+    const { searcheadItems, categoryPath, renderPages } = useContext(ShoppingCartContext)
     
+    const renderView = () => {
+        return(
+            categoryPath === 'All' 
+                ? 
+                    <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+                        {searcheadItems?.map((item) => (
+                            <Card 
+                            key={item.id}
+                            id={item.id}
+                            name={item.title}
+                            category={item.category}
+                            price={item.price}
+                            image={item.image}
+                            description={item.description}/>
+                        ))}            
+                    </div>
+                :
+                <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+                    {renderPages?.map((item) => (
+                        <Card 
+                        key={item.id}
+                        id={item.id}
+                        name={item.title}
+                        category={item.category}
+                        price={item.price}
+                        image={item.image}
+                        description={item.description}/>
+                    ))}            
+                </div> 
+        )
+    }
     return (
         <Layout>
             <SearchProducts title={'All Products'}/>
-            <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
-                {searcheadItems?.map((item) => (
-                    <Card 
-                    key={item.id}
-                    id={item.id}
-                    name={item.title}
-                    category={item.category}
-                    price={item.price}
-                    image={item.image}
-                    description={item.description}/>
-                ))}            
-            </div>            
+            {renderView()}  
         </Layout>
     )
 }
